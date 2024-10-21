2d63b4ece79fe71620d3b4dc11d58b93c36f06fc
soyab-mostofa <soyabmostofa@gmail.com>
2024-10-21 15:49:51 +0600

    fix route

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -35,6 +35,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://www.digigo.studio/"
+      : "https://digigo.studio"
   );
 }
