d3f3b038133774f55d3d6a09f09b398d880d5389
soyab-mostofa <soyabmostofa@gmail.com>
2024-10-21 14:56:11 +0600

    add domain to creation api

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -35,6 +35,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://inkam-ux-852558420668.asia-southeast1.run.app"
+      : "https://www.digigo.studio/"
   );
 }
