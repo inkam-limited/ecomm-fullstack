965af98b0990f10b5ccba17c88a2f8aea522aa34
soyab-mostofa <soyabmostofa@gmail.com>
2024-10-22 21:39:13 +0600

    Permanent redirect (308)

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,15 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
 
-  // Check if the app is running in production
+  // Ensure the redirection happens only in production
   if (process.env.NODE_ENV === "production") {
-    // If the hostname is not www and is not localhost (for dev purposes), then redirect
-    if (
-      !url.hostname.startsWith("www") &&
-      !url.hostname.includes("localhost")
-    ) {
-      // Redirect only if the hostname doesn't already start with 'www'
-      url.hostname = `www.${url.hostname}`;
-      return NextResponse.redirect(url, 308); // Permanent redirect (308)
+    // Check if the domain is non-www and not localhost
+    if (url.hostname === "digigo.studio") {
+      // Redirect to the www version of the site
+      url.hostname = "www.digigo.studio";
+      return NextResponse.redirect(url, 308); // Use 308 for permanent redirect
     }
   }
 
-  // Continue processing the request without redirection if the condition is not met
-  return NextResponse.next();
+  return NextResponse.next(); // Allow the request if conditions are not met
 }
